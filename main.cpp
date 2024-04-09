68e136f1ccdcee8973c973a3045bcf6b9e78baf9
Ajay Upadhyay <57ajay.u@gmail.com>
2024-04-09 12:50:29 +0530

    Refactored the code in main.cpp for better debugging

--- main.cpp
+++ main.cpp
@@ -1,42 +1,88 @@
 #include <iostream>
 using namespace std;
 
-int main (){
-    while(true){
-        cout<< "1: Print Help\n";
-        cout << "2: Print Exchange Stats\n";
-        cout << "3: Make an offer\n";
-        cout << "4: Make a bid\n";
-        cout << "5: Print Wallet\n";
-        cout << "6: Continue\n";
-        cout << "==================\n";
-        cout << "Type in 1-6: \n";
-        
-        int userOption;
-        cin >> userOption;
-        cout << "you chose "<< userOption << "\n";
+void printMenu(){
+    cout<< "1: Print Help\n";
+    cout << "2: Print Exchange Stats\n";
+    cout << "3: Make an offer\n";
+    cout << "4: Make a bid\n";
+    cout << "5: Print Wallet\n";
+    cout << "6: Continue\n";
+    cout << "==================\n";
+    
+};
 
+int getUserOption(){
+    cout << "Type in 1-6: \n";
+    int userOption;
+    cin >> userOption;
+    cout << "you chose "<< userOption << "\n";
+    return userOption;
+}
+
+void invalidInput(){
+    cout<< "Bad Input. Choose 1-6\n";
+}
+
+void printHelp(){
+    cout<< "Help: Your aim is to make money. Analyze the market and make bids and offers. \n";
+}
+
+void printMarketStats(){
+    cout<< "Market Stats: \n";
+}
+
+void printOffer(){
+    cout<< "Make an offer - enter the amount.\n";
+}
+
+void enterBid(){
+    cout<< "Make bid - enter the amount.\n"; 
+}
+
+void printWallet(){
+    cout<< "Wallet: \n";
+}
+
+void goToNextTimeFrame(){
+    cout<< "Going to next time frame.\n";
+}
+
+
+
+
+void processUserOption(int userOption){
+    
         if (userOption==0){
-            cout<< "Bad Input. Choose 1-6\n";
-            break;
+            invalidInput(); 
         };
         if (userOption==1){
-            cout<< "Help: Your aim is to make money. Analyze the market and make bids and offers. \n";
+            printHelp();
         };
         if (userOption==2){
-            cout<< "Market Looks Good.\n";
+            printMarketStats();   
         };
         if (userOption==3){
-            cout<< "Make an offer - enter the amount.\n";
+            printOffer(); 
         };
         if (userOption==4){
-            cout<< "Make bid - enter the amount.\n";
+            enterBid();
         };
         if (userOption==5){
-            cout<< "Your Wallet is empty.\n";
+            printWallet();   
         };
         if (userOption==6){
-            cout<< "Going to next time frame.\n";
+            goToNextTimeFrame();
+        };
+}
+int main (){
+
+    while(true){
+        printMenu();
+        int userOption = getUserOption();
+        processUserOption(userOption);
+        if (userOption==0){
+            break;
         };
     };
 
